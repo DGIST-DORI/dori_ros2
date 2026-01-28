a1fad2cf45b00b801cde37999c654c6e3ff42589
ofbt <jaewon1627@gmail.com>
2026-01-29 04:43:18 +0900

    Initialize cubesim_pkg with essential files and configurations

---
+++ src/cubesim_pkg/launch/cubesim.launch.py
@@ -0,0 +1,28 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    pkg_dir = get_package_share_directory('cubesim_pkg')
+    web_dir = os.path.join(pkg_dir, 'web')
+    
+    return LaunchDescription([
+        # rosbridge WebSocket server node
+        Node(
+            package='rosbridge_server',
+            executable='rosbridge_websocket',
+            name='rosbridge_websocket',
+            parameters=[{
+                'port': 9090,
+            }]
+        ),
+        
+        # React web serving by simple HTTP server
+        ExecuteProcess(
+            cmd=['python3', '-m', 'http.server', '3000'],
+            cwd=web_dir,
+            output='screen'
+        )
+    ])
---
+++ src/cubesim_pkg/setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+from glob import glob
+import os
+
+package_name = 'cubesim_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'web'), glob('web/dist/**/*', recursive=True)),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ofbt',
+    maintainer_email='jaewon1627@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'cube_bridge = cubesim_pkg.cube_bridge:main',
+        ],
+    },
+)
